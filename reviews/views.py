b27f17ed66d08e73636c083f8acce4c536b5f6d6
uRo3YA <ltg8693@gmail.com>
2022-10-14 14:15:33 +0900

    reviews/create

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Review
+from .forms import ReviewForm
 
 # Create your views here.
 def index(request):
@@ -8,3 +9,16 @@ def index(request):
         "contents": contents,
     }
     return render(request, "reviews/index.html", context)
+
+
+def create(request):
+    if request.method == "POST":
+        # DB에 저장하는 로직
+        review_form = ReviewForm(request.POST)
+        if review_form.is_valid():
+            review_form.save()
+            return redirect("reviews:index")
+    else:
+        review_form = ReviewForm()
+    context = {"review_form": review_form}
+    return render(request, "reviews/create.html", context=context)
