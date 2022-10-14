61eaff4ada9c7a6cd5a9fd0ed8a19e585a045878
uRo3YA <ltg8693@gmail.com>
2022-10-14 10:56:53 +0900

    accounts/update

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 
-# from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 from .models import User
+
 from .forms import CustomUserCreationForm
 from django.contrib.auth import get_user_model
 
@@ -10,7 +11,7 @@ from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 
-
+# Create your views here.
 def index(request):
     users = User.objects.all()
     # Template에 전달한다.
@@ -30,6 +31,12 @@ def signup(request):
     return render(request, "accounts/signup.html", context)
 
 
+def detail(request, pk):
+    user = get_user_model().objects.get(pk=pk)
+    context = {"user": user}
+    return render(request, "accounts/detail.html", context)
+
+
 def login(request):
     if request.method == "POST":
         # AuthenticationForm은 ModelForm이 아님!
@@ -41,15 +48,26 @@ def login(request):
             auth_login(request, form.get_user())
             # http://127.0.0.1:8000/accounts/login/?next=/articles/1/update/
             # request.GET.get('next') : /articles/1/update/
-            # return redirect(request.GET.get("next") or "articles:index")
-            return redirect("accounts:index")  # 임시 완성후 삭제 필요
+            return redirect(request.GET.get("next") or "articles:index")
     else:
         form = AuthenticationForm()
     context = {"form": form}
     return render(request, "accounts/login.html", context)
 
 
-def detail(request, pk):
-    user = get_user_model().objects.get(pk=pk)
-    context = {"user": user}
-    return render(request, "accounts/detail.html", context)
+def logout(request):
+    auth_logout(request)
+    return redirect("articles:index")
+
+
+@login_required
+def update(request):
+    if request.method == "POST":
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("accounts:detail", request.user.pk)
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {"form": form}
+    return render(request, "accounts/update.html", context)
